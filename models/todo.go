d3d522c0e37ee3d98144116824e0d3d07aa3dce5
kyo9bo <45879786+kk3939@users.noreply.github.com>
2022-12-31 22:33:06 +0900

    create todo models

--- models/todo.go
+++ models/todo.go
@@ -13,3 +13,36 @@ func GetTodos() (entity.ToDos, error) {
 	}
 	return todos, nil
 }
+
+func GetTodo(id string) (*entity.Todo, error) {
+	var todo entity.Todo
+	result := db.Db.First(&todo, "ID = ?", id)
+	if err := result.Error; err != nil {
+		return nil, err
+	}
+	return &todo, nil
+}
+
+func CreateTodo(td entity.Todo) (*entity.Todo, error) {
+	result := db.Db.Create(&td)
+	if err := result.Error; err != nil {
+		return nil, err
+	}
+	return &td, nil
+}
+
+func UpdateTodo(td entity.Todo) (*entity.Todo, error) {
+	result := db.Db.Save(&td)
+	if err := result.Error; err != nil {
+		return nil, err
+	}
+	return &td, nil
+}
+
+func DeleteTodo(td entity.Todo) error {
+	result := db.Db.Delete(&td)
+	if err := result.Error; err != nil {
+		return err
+	}
+	return nil
+}
