61aa9e902cae238d256e0ccb8fd95b3030554517
kyo9bo <45879786+kk3939@users.noreply.github.com>
2023-01-01 02:50:48 +0900

    create todo actions

--- controllers/todo.go
+++ controllers/todo.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/kk3939/gin-lime/entity"
 	"github.com/kk3939/gin-lime/models"
 )
 
@@ -16,4 +17,55 @@ func GetTodos(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, todos)
+}
+
+func GetTodo(c *gin.Context) {
+	id := c.Param("id")
+	todo, err := models.GetTodo(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": fmtErrMsg(err),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, todo)
+}
+
+func CreateTodo(c *gin.Context) {
+	var todo entity.Todo
+	c.BindJSON(&todo)
+	err := models.CreateTodo(todo)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": fmtErrMsg(err),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, todo)
+}
+
+func UpdateTodo(c *gin.Context) {
+	var todo entity.Todo
+	c.BindJSON(&todo)
+	err := models.UpdateTodo(todo)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": fmtErrMsg(err),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, todo)
+}
+
+func DeleteTodo(c *gin.Context) {
+	var todo entity.Todo
+	c.BindJSON(&todo)
+	err := models.DeleteTodo(todo)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": fmtErrMsg(err),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, todo)
 }
