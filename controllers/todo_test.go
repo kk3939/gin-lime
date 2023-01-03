b4e1885038d11ed7946cd8f97207a74c371bbfb9
kyo9bo <45879786+kk3939@users.noreply.github.com>
2023-01-03 19:03:40 +0900

    testing add getTodo controllers

--- controllers/todo_test.go
+++ controllers/todo_test.go
@@ -64,3 +64,52 @@ func Test_getTodos(t *testing.T) {
 		}
 	}
 }
+
+func Test_getTodo(t *testing.T) {
+	if _, _, err := db.Mock_DB(); err != nil {
+		t.Error("mock is failed")
+	}
+	mockDB, mock, _ := db.Mock_DB()
+	db.SetDB(mockDB)
+
+	todo := entity.Todo{
+		Id:      1,
+		Name:    "test",
+		Content: "test",
+	}
+	sql := "SELECT * FROM `todos` WHERE Id = ? AND `todos`.`deleted_at` IS NULL ORDER BY `todos`.`id` LIMIT 1"
+	mock.ExpectQuery(regexp.QuoteMeta(sql)).WithArgs().WillReturnRows(
+		sqlmock.NewRows([]string{
+			"Id",
+			"Name",
+			"Content",
+		}).AddRow(todo.Id, todo.Name, todo.Content))
+
+	r := server.GetRouter()
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/todo/1", nil)
+	r.ServeHTTP(w, req)
+	if w.Code != http.StatusOK {
+		t.Error("invalid request")
+	}
+	res := w.Result()
+	defer res.Body.Close()
+	if body, err := io.ReadAll(res.Body); err != nil {
+		t.Errorf("can not read res body. %v", err)
+	} else {
+		var td entity.Todo
+		if err := json.Unmarshal(body, &td); err != nil {
+			t.Errorf("can not map res json to struct. %v", err)
+		}
+		if todo.Id != td.Id {
+			fmt.Println(td.Id)
+			t.Error("id is not expected value")
+		}
+		if todo.Name != td.Name {
+			t.Error("Name is not expected value")
+		}
+		if todo.Content != td.Content {
+			t.Error("Content is not expected value")
+		}
+	}
+}
