bf3b44ffb65324a74b9f4812ce36ec60e8cd524f
kyo9bo <45879786+kk3939@users.noreply.github.com>
2023-01-03 19:27:08 +0900

    testing updateTodo controller

--- controllers/todo_test.go
+++ controllers/todo_test.go
@@ -169,3 +169,70 @@ func Test_createTodo(t *testing.T) {
 		}
 	}
 }
+
+func Test_updateTodo(t *testing.T) {
+	if _, _, err := db.Mock_DB(); err != nil {
+		t.Error("mock is failed")
+	}
+	mockDB, mock, _ := db.Mock_DB()
+	db.SetDB(mockDB)
+
+	todo := entity.Todo{
+		Id:      1,
+		Name:    "test",
+		Content: "test",
+	}
+
+	var sql string
+	sql = "SELECT * FROM `todos` WHERE Id = ? AND `todos`.`deleted_at` IS NULL ORDER BY `todos`.`id` LIMIT 1"
+	mock.ExpectQuery(regexp.QuoteMeta(sql)).WithArgs().WillReturnRows(
+		sqlmock.NewRows([]string{
+			"Id",
+			"Name",
+			"Content",
+		}).AddRow(todo.Id, todo.Name, todo.Content))
+
+	sql = "UPDATE `todos` SET `updated_at`=?,`name`=?,`content`=? WHERE `todos`.`deleted_at` IS NULL AND `id` = ?"
+	mock.ExpectBegin()
+	mock.ExpectExec(regexp.QuoteMeta(sql)).WithArgs(sqlmock.AnyArg(), todo.Name, todo.Content, todo.Id).WillReturnResult(sqlmock.NewResult(int64(todo.Id), 1))
+	mock.ExpectCommit()
+
+	r := server.GetRouter()
+	w := httptest.NewRecorder()
+	type Rq struct {
+		Name    string
+		Content string
+	}
+	rqb := Rq{
+		Name:    todo.Name,
+		Content: todo.Content,
+	}
+	if data, err := json.Marshal(rqb); err != nil {
+		t.Errorf("can not marshal json. %v", err)
+	} else {
+		req, _ := http.NewRequest("PUT", "/todo/1", bytes.NewBuffer(data))
+		r.ServeHTTP(w, req)
+	}
+	if w.Code != http.StatusOK {
+		t.Error("invalid request")
+	}
+	res := w.Result()
+	defer res.Body.Close()
+	if body, err := io.ReadAll(res.Body); err != nil {
+		t.Errorf("can not read res body. %v", err)
+	} else {
+		var td entity.Todo
+		if err := json.Unmarshal(body, &td); err != nil {
+			t.Errorf("can not map res json to struct. %v", err)
+		}
+		if todo.Id != td.Id {
+			t.Error("id is not expected value")
+		}
+		if todo.Name != td.Name {
+			t.Error("Name is not expected value")
+		}
+		if todo.Content != td.Content {
+			t.Error("Content is not expected value")
+		}
+	}
+}
