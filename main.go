2d804510cd748c0371b0d01aee96dd0b0db1c6ba
kyo9bo <45879786+kk3939@users.noreply.github.com>
2023-01-03 20:21:01 +0900

    refactor env if statement

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/kk3939/gin-lime/db"
@@ -10,9 +11,16 @@ import (
 func main() {
 	rt := 30
 	db.Connect(rt)
-	if os.Getenv("APP_ENV") == "dev" {
+	if os.Getenv("APP_ENV") == "local" {
+		fmt.Println("This is local env.")
 		var seeds db.Seeds
 		seeds.ToDo()
+	} else if os.Getenv("APP_ENV") == "dev" {
+		fmt.Println("This is staging env.")
+	} else if os.Getenv("APP_ENV") == "prod" {
+		fmt.Println("This is production env.")
+	} else {
+		panic("APP_ENV environmental variable is not considered value.")
 	}
 	r := server.GetRouter()
 	r.Run(":3000")
