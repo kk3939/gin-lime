9fcd130f05cc3d660e0944066a1052d2f280e01b
kyo9bo <45879786+kk3939@users.noreply.github.com>
2023-01-03 19:26:40 +0900

    testing createTodo controller

--- controllers/todo_test.go
+++ controllers/todo_test.go
@@ -1,8 +1,8 @@
 package controllers_test
 
 import (
+	"bytes"
 	"encoding/json"
-	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -53,7 +53,6 @@ func Test_getTodos(t *testing.T) {
 			t.Errorf("can not map res json to struct. %v", err)
 		}
 		if todo.Id != td[0].Id {
-			fmt.Println(td[0].Id)
 			t.Error("id is not expected value")
 		}
 		if todo.Name != td[0].Name {
@@ -102,7 +101,64 @@ func Test_getTodo(t *testing.T) {
 			t.Errorf("can not map res json to struct. %v", err)
 		}
 		if todo.Id != td.Id {
-			fmt.Println(td.Id)
+			t.Error("id is not expected value")
+		}
+		if todo.Name != td.Name {
+			t.Error("Name is not expected value")
+		}
+		if todo.Content != td.Content {
+			t.Error("Content is not expected value")
+		}
+	}
+}
+
+func Test_createTodo(t *testing.T) {
+	if _, _, err := db.Mock_DB(); err != nil {
+		t.Error("mock is failed")
+	}
+	mockDB, mock, _ := db.Mock_DB()
+	db.SetDB(mockDB)
+
+	todo := entity.Todo{
+		Id:      1,
+		Name:    "test",
+		Content: "test",
+	}
+
+	sql := "INSERT INTO `todos` (`created_at`,`updated_at`,`name`,`content`) VALUES (?,?,?,?)"
+	mock.ExpectBegin()
+	mock.ExpectExec(regexp.QuoteMeta(sql)).WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), todo.Name, todo.Content).WillReturnResult(sqlmock.NewResult(int64(todo.Id), 1))
+	mock.ExpectCommit()
+
+	r := server.GetRouter()
+	w := httptest.NewRecorder()
+	type Rq struct {
+		Name    string
+		Content string
+	}
+	rqb := Rq{
+		Name:    todo.Name,
+		Content: todo.Content,
+	}
+	if data, err := json.Marshal(rqb); err != nil {
+		t.Errorf("can not marshal json. %v", err)
+	} else {
+		req, _ := http.NewRequest("POST", "/todo", bytes.NewBuffer(data))
+		r.ServeHTTP(w, req)
+	}
+	if w.Code != http.StatusOK {
+		t.Error("invalid request")
+	}
+	res := w.Result()
+	defer res.Body.Close()
+	if body, err := io.ReadAll(res.Body); err != nil {
+		t.Errorf("can not read res body. %v", err)
+	} else {
+		var td entity.Todo
+		if err := json.Unmarshal(body, &td); err != nil {
+			t.Errorf("can not map res json to struct. %v", err)
+		}
+		if todo.Id != td.Id {
 			t.Error("id is not expected value")
 		}
 		if todo.Name != td.Name {
