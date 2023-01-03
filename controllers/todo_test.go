e872e6947acb894493c9278acc6b93122e571f57
kyo9bo <45879786+kk3939@users.noreply.github.com>
2023-01-03 19:39:56 +0900

    testing deleteTodo controllers

--- controllers/todo_test.go
+++ controllers/todo_test.go
@@ -236,3 +236,64 @@ func Test_updateTodo(t *testing.T) {
 		}
 	}
 }
+
+func Test_deleteTodo(t *testing.T) {
+	if _, _, err := db.Mock_DB(); err != nil {
+		t.Error("mock is failed")
+	}
+	mockDB, mock, _ := db.Mock_DB()
+	db.SetDB(mockDB)
+
+	todo := entity.Todo{
+		Id:      1,
+		Name:    "test",
+		Content: "test",
+	}
+
+	var sql string
+	sql = "SELECT * FROM `todos` WHERE Id = ? AND `todos`.`deleted_at` IS NULL ORDER BY `todos`.`id` LIMIT 1"
+	mock.ExpectQuery(regexp.QuoteMeta(sql)).WithArgs().WillReturnRows(
+		sqlmock.NewRows([]string{
+			"Id",
+			"Name",
+			"Content",
+		}).AddRow(todo.Id, todo.Name, todo.Content))
+
+	sql = "UPDATE `todos` SET `deleted_at`=? WHERE `todos`.`id` = ? AND `todos`.`deleted_at` IS NULL"
+	mock.ExpectBegin()
+	mock.ExpectExec(regexp.QuoteMeta(sql)).WithArgs(sqlmock.AnyArg(), todo.Id).WillReturnResult(sqlmock.NewResult(int64(todo.Id), 1))
+	mock.ExpectCommit()
+
+	r := server.GetRouter()
+	w := httptest.NewRecorder()
+
+	req, _ := http.NewRequest("DELETE", "/todo/1", nil)
+	r.ServeHTTP(w, req)
+
+	if w.Code != http.StatusOK {
+		t.Error("invalid request")
+	}
+	res := w.Result()
+	defer res.Body.Close()
+	if body, err := io.ReadAll(res.Body); err != nil {
+		t.Errorf("can not read res body. %v", err)
+	} else {
+		var td entity.Todo
+		if err := json.Unmarshal(body, &td); err != nil {
+			t.Errorf("can not map res json to struct. %v", err)
+		}
+
+		if todo.Id != td.Id {
+			t.Error("id is not expected value")
+		}
+		if td.Name != "" {
+			t.Error("Name is not expected value")
+		}
+		if td.Content != "" {
+			t.Error("Content is not expected value")
+		}
+		if todo.DeletedAt.Valid {
+			t.Error("DeletedAt is not expected value")
+		}
+	}
+}
